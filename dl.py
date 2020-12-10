b728ba5ff43d8b7b0c9521c21fcfa2093d428e87
Khadidja CH <39216434+KhadidjaCroft@users.noreply.github.com>
2020-12-10 19:15:01 +0100

    Update dl.py

--- dl.py
+++ dl.py
@@ -50,3 +50,9 @@ class Dataset(dataset):
             img = self.transform(img)
 
         return img,cls
+    
+#creating and testing a transform method
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+composed = transforms.Compose([transforms.Resize((224, 224)),transforms.ToTensor(), transforms.Normalize(mean, std)])
+test_dataset=dataset(train_data_name,data_dir,transform=composed)
